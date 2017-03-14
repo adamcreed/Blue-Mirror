c48abe3c0c92755d0163bfa2bdbf3a2d87e7c781
katherine-hill <katherinehill1991@gmail.com>
2017-03-14 17:49:26 -0400

    commented out map controller

--- app/assets/javascripts/controllers/map-controller.js
+++ app/assets/javascripts/controllers/map-controller.js
@@ -1,6 +1,6 @@
-(function(ng, currentUser) {
-    ng.module('BlueMirrorApp', ['google-maps'])
-        .controller('MapController', function($state, $scope, $q, DataRequestService, UserService) {
-
-        });
-})(angular);
+// (function(ng, currentUser) {
+//     ng.module('BlueMirrorApp', ['google-maps'])
+//         .controller('MapController', function($state, $scope, $q, DataRequestService, UserService) {
+//
+//         });
+// })(angular);
