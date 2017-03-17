2b8a0e03ab3dd339126025e192f6ed0522e8c305
Adam Reed <kusa.xisa@gmail.com>
2017-03-17 15:56:42 -0400

    Added featured todos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def featured_todos
+    @todos = []
+    File.readlines('db/data/todos.seed', quote_char: "\x00").each do |row|
+      @todos << row.strip
+    end
+    @todos << Todo.where('featured = ?', true).pluck('todo')
+    @todos.flatten.uniq
+  end
 end
