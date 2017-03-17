72023165b055f8b5c667c70c38f331a58a77e9a7
Amiee <amiee.marielo@gmail.com>
2017-03-17 17:39:03 -0400

    added labels and data array for chart js

--- app/assets/javascripts/controllers/profile-controller.js
+++ app/assets/javascripts/controllers/profile-controller.js
@@ -16,8 +16,21 @@
         // get moods
         $q.when(DataRequestService.get('/moods')).then((response) => {
 
-            $scope.currentMood = response.data;
-            console.log($scope.currentMood);
+            $scope.allMoods = response.data;
+
+            $scope.labels = [];
+            $scope.data = [];
+
+            for (let i = 0; i < $scope.allMoods.length; i++) {
+                $scope.labels.push($scope.allMoods[i].created_at);
+                $scope.data.push($scope.allMoods[i].mood);
+            }
+            $scope.data = [$scope.data];
+
+            console.log($scope.labels);
+            console.log($scope.data);
+
+            // console.log($scope.allMoods);
 
         }).catch((error) => {
             console.log(error);
@@ -61,15 +74,12 @@
 
         // CHART MOODS
 
-        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
         $scope.series = ['Series A'];
-        $scope.data = [
-            [65, 59, 80, 81, 56, 55, 40]
-        ];
 
         $scope.onClick = function(points, evt) {
             console.log(points, evt);
         };
+
 
         $scope.datasetOverride = [{
             yAxisID: 'y-axis-1'
