eadea3f4197e1500af146e27f96d20bd908ba520
Adam Reed <kusa.xisa@gmail.com>
2017-03-26 19:59:20 -0400

    Fixed tests to run in any order

--- test/helpers/capybara_test_helper.rb
+++ test/helpers/capybara_test_helper.rb
@@ -2,13 +2,14 @@ module CapybaraTestHelper
   def login(username:, password:)
     visit root_path
     click_button 'Sign in with Google'
+    if page.has_css? '#Email'
+      fill_in 'Email', with: username
+      click_on 'next'
 
-    fill_in 'Email', with: username
-    click_on 'next'
-
-    fill_in 'Passwd', with: password
-    click_on 'signIn'
-    sleep 3
-    click_on 'submit_approve_access'
+      fill_in 'Passwd', with: password
+      click_on 'signIn'
+      sleep 3
+      click_on 'submit_approve_access'
+    end
   end
 end
