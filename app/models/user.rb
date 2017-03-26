3d8f4a15426a514b40383ec94116eb18d6fdf535
Adam Reed <kusa.xisa@gmail.com>
2017-03-26 00:18:05 -0400

    Added hash to obfuscate uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,23 @@
 class User < ApplicationRecord
   include Seed
 
+  MY_SALT = ENV['SALT']
+
+  # before_create :set_hashed_id
+  #
+  # def set_hashed_id
+  #   self.uid = User.get_hash uid
+  # end
+
+  def self.get_hash(uid)
+    Digest::SHA1.hexdigest("--#{MY_SALT}--#{uid}--")
+  end
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+    hashed_id = get_hash auth.uid
+    where(provider: auth.provider, uid: hashed_id).first_or_initialize.tap do |user|
       user.provider = auth.provider
-      user.uid = auth.uid
+      user.uid = hashed_id
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
