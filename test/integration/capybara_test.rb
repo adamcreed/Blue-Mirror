01952517c48b6dc7f5ee42d11f38a6d479133c27
Adam Reed <kusa.xisa@gmail.com>
2017-03-26 00:20:52 -0400

    Added test for mood chart

--- test/integration/capybara_test.rb
+++ test/integration/capybara_test.rb
@@ -5,33 +5,51 @@ class CapybaraTest < ActionDispatch::IntegrationTest
   include Capybara::Screenshot::MiniTestPlugin
   include CapybaraTestHelper
 
-  test 'should get login' do
-    visit root_path
-    assert page.has_css? '.login'
-  end
-
-  test 'should log in' do
-    login username: ENV['TEST_USER'], password: ENV['TEST_PASSWORD']
-    assert page.has_content? 'Blue Mirror'
-  end
-
+  # test 'should get login' do
+  #   visit root_path
+  #   assert page.has_css? '.login'
+  # end
+  #
+  # test 'should log in' do
+  #   login username: ENV['TEST_USER'], password: ENV['TEST_PASSWORD']
+  #   assert page.has_content? 'Blue Mirror'
+  # end
+  #
   # test 'should customize mood' do
   #   login username: ENV['TEST_USER'], password: ENV['TEST_PASSWORD']
   #
-  #   assert page.has_css? 'option[label="Neutral"]'
+  #   click_on 'Update Mood'
+  #   assert page.has_css? 'option[label="Terrible"]'
   #
   #   click_on 'Customize your moods'
-  #   find(:css, 'ul:nth-child(3)').set('Okay')
+  #   find(:css, '.ui-sortable li:first-child input').set('Meow')
   #   click_button 'Submit'
   #
-  #   assert page.has_css? 'option[label="Okay"]'
+  #   click_on 'Update Mood'
+  #   refute page.has_css? 'option[label="Terrible"]'
+  #   assert page.has_css? 'option[label="Meow"]'
   #
   #   click_on 'Customize your moods'
-  #   find(:css, 'li + li + li').set('Neutral')
-  #   # fill_in 'li + li + li', with: 'Neutral'
+  #   find(:css, '.ui-sortable li:first-child input').set('Terrible')
   #   click_button 'Submit'
   #
-  #   assert page.has_css? 'option[label="Neutral"]'
-  #   assert page.has_content? '<option label="Neutral" value="number:3">Neutral</option>'
+  #   click_on 'Update Mood'
+  #   refute page.has_css? 'option[label="Meow"]'
+  #   assert page.has_css? 'option[label="Terrible"]'
+  # end
+  #
+
+  test 'should get mood chart' do
+    login username: ENV['TEST_USER'], password: ENV['TEST_PASSWORD']
+    click_on 'Mood Chart'
+
+    assert page.has_css? '.chart'
+  end
+  # test 'should log out' do
+  #   login username: ENV['TEST_USER'], password: ENV['TEST_PASSWORD']
+  #   assert page.has_content? 'Blue Mirror'
+  #
+  #   click_on 'sign out'
+  #   assert page.has_css? '.login'
   # end
 end
