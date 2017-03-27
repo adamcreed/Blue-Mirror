0ab9b7694e36472c9560bf16408d4d58b9c0567b
Amiee <amiee.marielo@gmail.com>
2017-03-27 18:32:04 -0400

    added jQuery touch punch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery/dist/jquery
 //= require angular/angular
 //= require jquery-ui/jquery-ui
+//= require jqueryui-touch-punch/jquery.ui.touch-punch.min
 //= require angular-rails-templates
 //= require angular-ui-router/release/angular-ui-router
 //= require angular-simple-logger/dist/angular-simple-logger
