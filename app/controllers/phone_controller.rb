15680295a2b776602fe806248fdea5a64318ccc7
Amiee <amiee.marielo@gmail.com>
2017-03-28 12:14:08 -0400

    setting up sms to send on front end

--- app/controllers/phone_controller.rb
+++ app/controllers/phone_controller.rb
@@ -1,14 +1,18 @@
-def update
-  @user = User.find_by_id(current_user.id)
+class PhoneController < ApplicationController
+    def update
+      @user = User.find_by_id(current_user.id)
 
-  if @user.update(phone_params)
-    render json: { location: @user }
-  else
-    render json: @user.errors, status: :unprocessable_entity
-  end
-end
+      if @user.update(phone_params)
+        render json: { location: @user }
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
 
-def phone_params
-  params[:phone] = encrypt(params[:phone].gsub(/\D/, ''))
-  params.permit(:phone, :phone_provider, :sms_frequency, :user_id)
+    private
+
+    def phone_params
+      params[:phone] = encrypt(params[:phone].gsub(/\D/, ''))
+      params.permit(:phone, :phone_provider, :sms_frequency, :user_id)
+    end
 end
