a55b9da424eeac6274a7946a509ab06e339c0173
katherine-hill <katherinehill1991@gmail.com>
2017-03-29 19:16:05 -0400

    mobile nav ng-hide/ng-showing

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
   before_action :set_note, only: [:show, :update, :destroy]
-
+include ApplicationHelper
   # GET /notes
   # GET /notes.json
   def index
